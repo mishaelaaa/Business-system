b3242e2d8a7cdc9e37e433d5f861ddce4c8505a0
Mihaela Petkova <mishippp@gmail.com>
2021-03-16 08:44:37 +0200

    adding comments

--- Business-system/Business-system/database_accsses.py
+++ Business-system/Business-system/database_accsses.py
@@ -1,8 +1,11 @@
 import pyodbc
+###lib for connect python code and accsses database 
 
 conn = pyodbc.connect(r'Driver={Microsoft Access Driver (*.mdb, *.accdb)};DBQ=F:\Mihaela\PROJECTS\Kasi Tempra Pak\QR_Code_generator\Business-system\database\test_1.accdb;')
+### parth with source 
 cursor = conn.cursor()
 cursor.execute('select * from Table1')
-   
+### choose the table 
+
 for row in cursor.fetchall():
     print (row)
